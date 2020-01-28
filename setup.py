b03f0b3d1d99ab2087a2a6cb4281811e0883f963
Ruben Purdy <rpurdy@andrew.cmu.edu>
2020-01-28 15:55:11 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="truthtables", # Replace with your own username
+    version="0.0.1",
+    author="Ruben Purdy",
+    author_email="rpurdy@andrew.cmu.edu",
+    description="A package to facilitate working with truth tables in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rbnprdy/truthtables",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
