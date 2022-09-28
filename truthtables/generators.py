b98ed7aa250dd5808b3f95208ef5927fbd78ceb1
RiceNinja97 <littleninja9@gmail.com>
2022-09-28 14:18:18 -0400

    added bias

--- truthtables/generators.py
+++ truthtables/generators.py
@@ -7,16 +7,16 @@ from truthtables import TruthTable
 # add bias (between 0 and 1)
 def random_table(num_inputs, num_outputs, bias):
     lines = []
-
+    val = 0
     for _ in range(2**num_inputs):
+        val = 0
         for a in range(num_outputs):
-            x = random.random()
-            if (x > bias):
-                lines.append(1)
-            else:
-                lines.append(0)
+           # bias = 0 => use all 1s
+            x = [0, 1][random.randrange(0, 10) > bias]
+            val = (val << 1) or x
+        lines.append(bin(val)[2:].zfill(num_outputs))
         # generates a random number with num_outputs # of bits
         # cuts off "0b" because using bin function
         # zfill adds 0 until num_bits is full
-        #lines.append(bin(random.getrandbits(num_outputs))[2:].zfill(num_outputs))
+        # lines.append(bin(random.getrandbits(num_outputs))[2:].zfill(num_outputs))
     return TruthTable(lines)
