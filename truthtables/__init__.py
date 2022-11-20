b2fbdcb3c9c9bca1c762cc3f65fdcf0ca93c1905
RiceNinja97 <littleninja9@gmail.com>
2022-11-20 00:58:34 -0500

    Added file to truth table function

--- truthtables/__init__.py
+++ truthtables/__init__.py
@@ -2,3 +2,4 @@ from truthtables.truthtable import TruthTable, PLA
 from truthtables.io import to_file
 from truthtables.generators import random_table
 from truthtables.transforms import minimize
+from truthtables.generators import file_table
--- truthtables/generators.py
+++ truthtables/generators.py
@@ -1,9 +1,50 @@
 """Functions for generating truth tables"""
 
 import random
+import ast
 
 from truthtables import TruthTable
 
+def file_table(filename, num_inputs, num_outputs, bias=0.5):
+
+    dict_list = []
+    rows = []
+
+    f = open(filename, 'r')
+    lines = f.readlines()
+
+    for line in lines:
+        d = line.strip()
+        d = d.replace(" ", "")
+        d = d.replace("\n", "")
+        d = d.replace("{", "")
+        d = d.replace("}", "")
+        #print("Hello")
+        #print(d)
+        data = dict(item.split(":") for item in d.split(","))
+        #for item in d.split(","):
+        #    print(item.split(":"))
+        dict_list.append(data)
+
+    num_input_combo = len(dict_list)
+    num_outputs = len(dict_list[0])
+
+    for i in range(num_input_combo): # loop through dict_list
+        val = 0
+        rand_val = 0
+        for key in dict_list[i]:
+            #print(type(dict_list[i][key]))
+            if (dict_list[i][key] == "False"):
+                sv = 0
+            else:
+                sv = 1
+            val = (val << 1) | sv
+            x = [0, 1][random.random() < bias]
+            #rand_val = (rand_val << 1) | x
+        #val = (val ^ rand_val)
+        rows.append(bin(val)[2:].zfill(num_outputs))
+
+    return TruthTable(rows)
 
 def random_table(num_inputs, num_outputs, bias=0.5):
     """Generate a random table.
